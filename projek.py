a1500cacd99e06e42f9da1f1123cf817b176d2d1
scarlettgrey <58540429+scarlettgrey@users.noreply.github.com>
2020-09-30 10:08:25 +0700

    update partition, hdd recovery and format
    partition = add letter disk available
    hdd recovery and format = add select folder(future update change to select disk)

--- projek.py
+++ projek.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import ttk,filedialog
+from ctypes import windll
+import string,os
 
 window = tk.Tk()
 window.title("BitRecov")
@@ -7,14 +9,26 @@ window.geometry("600x400")
 
 Ptab = ttk.Notebook(window)
 
+def openDirHDDrecov():
+    DirHDDRecov = filedialog.askdirectory()
+    EntryTargetDiskHDDRecov.insert(0,DirHDDRecov)
+
 tabHDD = tk.Frame(Ptab)
 Ptab.add(tabHDD,text="HDD Reovery")
 LabelTargetDiskHDDRecov = tk.Label(tabHDD,text="Target Disk: ")
 LabelTargetDiskHDDRecov.grid(row=0,column=0,pady=5,sticky="W")
 EntryTargetDiskHDDRecov = tk.Entry(tabHDD)
 EntryTargetDiskHDDRecov.grid(row=0,column=1,columnspan=2,ipadx=165,pady=5,sticky="E")
+tk.Button(tabHDD,text="...",command=openDirHDDrecov).grid(row=0,column=2,ipadx=5,pady=5,sticky="E")
 tk.Checkbutton(tabHDD,text="Generate Report").grid(row=1,column=0,columnspan=2,sticky="W")
 tk.Button(tabHDD,text="Start Recovery").grid(row=1,column=2,sticky="E")
+
+Letter = ['A:','B:','C:','D:','E:','F:','G:','H:','I:','J:','K:','L:','M:','N:','O:','P:','Q:','R:','S:','T:','U:','V:','W:','X:','Y:','Z:']
+OccupiedDrivesLetter = ['%s:'%d for d in string.ascii_uppercase if os.path.exists('%s:'%d)]
+AvailableDrivesLetter = []
+for a in Letter:
+    if a not in OccupiedDrivesLetter:
+        AvailableDrivesLetter.append(a)
 
 tabPartition = tk.Frame(Ptab)
 Ptab.add(tabPartition,text="Partition Tools")
@@ -33,10 +47,14 @@ EntrySizePartition = tk.Entry(tabPartition)
 EntrySizePartition.grid(row=4,column=0,columnspan=2,ipadx=25,sticky="E")
 LabelPartitionLetter = tk.Label(tabPartition,text="Partition Letter: ")
 LabelPartitionLetter.grid(row=4,column=2,sticky="W")
-EntryPartitionLetter = tk.Entry(tabPartition)
-EntryPartitionLetter.grid(row=4,column=2,columnspan=2,ipadx=32,sticky="E")
+EntryPartitionLetter = ttk.Combobox(tabPartition,values=AvailableDrivesLetter)
+EntryPartitionLetter.grid(row=4,column=2,columnspan=2,ipadx=25,sticky="E")
 tk.Checkbutton(tabPartition,text="Generate Report").grid(row=5,column=0)
 tk.Button(tabPartition,text="Execute").grid(row=5,column=3,sticky="E")
+
+def openDirFormat():
+    DirFormat = filedialog.askdirectory()
+    EntryTargetDiskFormat.insert(0,DirFormat)
 
 tabFormat = tk.Frame(Ptab)
 Ptab.add(tabFormat,text="Format")
@@ -44,6 +62,7 @@ LabelTargetDiskFormat = tk.Label(tabFormat,text="Target Disk/Partition: ")
 LabelTargetDiskFormat.grid(row=0,column=0,pady=5)
 EntryTargetDiskFormat = tk.Entry(tabFormat)
 EntryTargetDiskFormat.grid(row=0,column=1,columnspan=2,ipadx=140,pady=5,sticky="E")
+tk.Button(tabFormat,text="...",command=openDirFormat).grid(row=0,column=2,ipadx=5,pady=5,sticky="E")
 tk.Checkbutton(tabFormat,text="Generate Report").grid(row=1,column=0)
 tk.Button(tabFormat,text="Start Format").grid(row=1,column=2,sticky="E")
 
@@ -55,7 +74,6 @@ def openfileEnc():
 tabEnc = tk.Frame(Ptab)
 Ptab.add(tabEnc,text="Encryption")
 LabelTargetDataEnc = tk.Label(tabEnc,text="Target Data: ")
-tk.Button(tabEnc,text="...",command=openfileEnc).grid(row=0,column=2,ipadx=5,pady=5,sticky="E")
 LabelTargetDataEnc.grid(row=0,column=0,pady=5,sticky="W")
 EntryTargetDataEnc = tk.Entry(tabEnc)
 EntryTargetDataEnc.grid(row=0,column=1,columnspan=2,ipadx=144,pady=5,sticky="E")
