bfaa574bd1c9ae829b2286184f9900b442270859
scarlettgrey <58540429+scarlettgrey@users.noreply.github.com>
2020-09-30 10:19:38 +0700

    update hdd recov and format
    now you can select disk drive instead of selecting folders

--- projek.py
+++ projek.py
@@ -9,26 +9,21 @@ window.geometry("600x400")
 
 Ptab = ttk.Notebook(window)
 
-def openDirHDDrecov():
-    DirHDDRecov = filedialog.askdirectory()
-    EntryTargetDiskHDDRecov.insert(0,DirHDDRecov)
-
-tabHDD = tk.Frame(Ptab)
-Ptab.add(tabHDD,text="HDD Reovery")
-LabelTargetDiskHDDRecov = tk.Label(tabHDD,text="Target Disk: ")
-LabelTargetDiskHDDRecov.grid(row=0,column=0,pady=5,sticky="W")
-EntryTargetDiskHDDRecov = tk.Entry(tabHDD)
-EntryTargetDiskHDDRecov.grid(row=0,column=1,columnspan=2,ipadx=165,pady=5,sticky="E")
-tk.Button(tabHDD,text="...",command=openDirHDDrecov).grid(row=0,column=2,ipadx=5,pady=5,sticky="E")
-tk.Checkbutton(tabHDD,text="Generate Report").grid(row=1,column=0,columnspan=2,sticky="W")
-tk.Button(tabHDD,text="Start Recovery").grid(row=1,column=2,sticky="E")
-
 Letter = ['A:','B:','C:','D:','E:','F:','G:','H:','I:','J:','K:','L:','M:','N:','O:','P:','Q:','R:','S:','T:','U:','V:','W:','X:','Y:','Z:']
 OccupiedDrivesLetter = ['%s:'%d for d in string.ascii_uppercase if os.path.exists('%s:'%d)]
 AvailableDrivesLetter = []
 for a in Letter:
     if a not in OccupiedDrivesLetter:
         AvailableDrivesLetter.append(a)
+
+tabHDD = tk.Frame(Ptab)
+Ptab.add(tabHDD,text="HDD Reovery")
+LabelTargetDiskHDDRecov = tk.Label(tabHDD,text="Target Disk: ")
+LabelTargetDiskHDDRecov.grid(row=0,column=0,pady=5,sticky="W")
+EntryTargetDiskHDDRecov = ttk.Combobox(tabHDD,values=OccupiedDrivesLetter)
+EntryTargetDiskHDDRecov.grid(row=0,column=1,columnspan=2,ipadx=25,sticky="E")
+tk.Checkbutton(tabHDD,text="Generate Report").grid(row=1,column=0,columnspan=2,sticky="W")
+tk.Button(tabHDD,text="Start Recovery").grid(row=1,column=2,sticky="E")
 
 tabPartition = tk.Frame(Ptab)
 Ptab.add(tabPartition,text="Partition Tools")
@@ -52,17 +47,12 @@ EntryPartitionLetter.grid(row=4,column=2,columnspan=2,ipadx=25,sticky="E")
 tk.Checkbutton(tabPartition,text="Generate Report").grid(row=5,column=0)
 tk.Button(tabPartition,text="Execute").grid(row=5,column=3,sticky="E")
 
-def openDirFormat():
-    DirFormat = filedialog.askdirectory()
-    EntryTargetDiskFormat.insert(0,DirFormat)
-
 tabFormat = tk.Frame(Ptab)
 Ptab.add(tabFormat,text="Format")
 LabelTargetDiskFormat = tk.Label(tabFormat,text="Target Disk/Partition: ")
 LabelTargetDiskFormat.grid(row=0,column=0,pady=5)
-EntryTargetDiskFormat = tk.Entry(tabFormat)
-EntryTargetDiskFormat.grid(row=0,column=1,columnspan=2,ipadx=140,pady=5,sticky="E")
-tk.Button(tabFormat,text="...",command=openDirFormat).grid(row=0,column=2,ipadx=5,pady=5,sticky="E")
+EntryTargetDiskFormat = ttk.Combobox(tabFormat,values=OccupiedDrivesLetter)
+EntryTargetDiskFormat.grid(row=0,column=1,columnspan=2,ipadx=25,sticky="E")
 tk.Checkbutton(tabFormat,text="Generate Report").grid(row=1,column=0)
 tk.Button(tabFormat,text="Start Format").grid(row=1,column=2,sticky="E")
 
